56973aa76ebfb180e1b1e54d756eaac2f103eb2d
Tjorve Kujath <tjorve.kujath@dlr.de>
2019-04-26 13:42:37 +0200

    Refactoring the Test cases  - (Task #11)
    Delete Code doubling.
    
    ---
    Task #11: Execution of OverTarget in Maven/Tycho gives warning

--- de.dlr.sc.overtarget.language.tests/src/de/dlr/sc/overtarget/language/tests/OvertargetOutputConfigurationProviderTest.java
+++ de.dlr.sc.overtarget.language.tests/src/de/dlr/sc/overtarget/language/tests/OvertargetOutputConfigurationProviderTest.java
@@ -12,16 +12,16 @@ package de.dlr.sc.overtarget.language.tests;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.Set;
 
-import org.eclipse.xtext.generator.IFileSystemAccess;
 import org.eclipse.xtext.generator.OutputConfiguration;
 import org.junit.Test;
 
 import de.dlr.sc.overtarget.language.generator.OvertargetOutputConfigurationProvider;
 /**
- * Testcase for OvertargetOutputConfigurationProvider
+ * Test Case for OvertargetOutputConfigurationProvider
  * @author kuja_tj
  *
  */
@@ -29,42 +29,33 @@ public class OvertargetOutputConfigurationProviderTest {
 
 	@Test
 	public void testGetOutputConfigurations() {
-		final int NUMBER_OF_OUTPUTS = 2;
 
-		final String GENERATOR_OUTPUT_ID_OVERTARGET = "de.dlr.sc.overtarget.output";
-		final String GENERATOR_OUTPUT_FOLDER_CONCEPT = "./";
-		final String GENERATOR_OUTPUT_DESCRIPTION_CONCEPT = "Xtend Overtarget Serialzation Output";
-
-		final String GENERATOR_OUTPUT_ID_DEFAULT = IFileSystemAccess.DEFAULT_OUTPUT;
-		final String GENERATOR_OUTPUT_FOLDER_DEFAULT = "./default-gen";
-		final String GENERATOR_OUTPUT_DESCRIPTION_DEFAULT = "Xtend Default Output";
-		
 		OvertargetOutputConfigurationProvider oocp = new OvertargetOutputConfigurationProvider();
 		Set<OutputConfiguration> outputConfigurations = oocp.getOutputConfigurations();
 		
-		assertEquals("The number of Output Configurations is correct", NUMBER_OF_OUTPUTS, outputConfigurations.size());
-
+		assertEquals("The number of Output Configurations is correct", 2, outputConfigurations.size());
+		
 		for (OutputConfiguration outputConf : outputConfigurations) {
 			String confName = outputConf.getName();
 			switch (confName) {
-				case GENERATOR_OUTPUT_ID_OVERTARGET:
-					assertEquals("Description is set correctly", GENERATOR_OUTPUT_DESCRIPTION_CONCEPT, outputConf.getDescription());
-					assertEquals("Folder is set correctly", GENERATOR_OUTPUT_FOLDER_CONCEPT, outputConf.getOutputDirectory());
+				case OvertargetOutputConfigurationProvider.GENERATOR_OUTPUT_ID_OVERTARGET:
+					assertEquals("Description is set correctly", OvertargetOutputConfigurationProvider.GENERATOR_OUTPUT_DESCRIPTION_OVERTARGET, outputConf.getDescription());
+					assertEquals("Folder is set correctly", OvertargetOutputConfigurationProvider.GENERATOR_OUTPUT_FOLDER_CONCEPT, outputConf.getOutputDirectory());
 					assertTrue("OverrideExistingResources is set correctly", outputConf.isOverrideExistingResources());
 					assertTrue("CreateOutputDirectory is set correctly", outputConf.isCreateOutputDirectory());
 					assertFalse("CleanUpDerivedResources is set correctly", outputConf.isCleanUpDerivedResources());
 					assertFalse("SetDerivedProperty is set correctly", outputConf.isSetDerivedProperty());
 					break;
-				case GENERATOR_OUTPUT_ID_DEFAULT:
-					assertEquals("Description is set correctly", GENERATOR_OUTPUT_DESCRIPTION_DEFAULT, outputConf.getDescription());
-					assertEquals("Folder is set correctly", GENERATOR_OUTPUT_FOLDER_DEFAULT, outputConf.getOutputDirectory());
+				case OvertargetOutputConfigurationProvider.GENERATOR_OUTPUT_ID_DEFAULT:
+					assertEquals("Description is set correctly", OvertargetOutputConfigurationProvider.GENERATOR_OUTPUT_DESCRIPTION_DEFAULT, outputConf.getDescription());
+					assertEquals("Folder is set correctly", OvertargetOutputConfigurationProvider.GENERATOR_OUTPUT_FOLDER_DEFAULT, outputConf.getOutputDirectory());
 					assertTrue("OverrideExistingResources is set correctly", outputConf.isOverrideExistingResources());
 					assertTrue("CreateOutputDirectory is set correctly", outputConf.isCreateOutputDirectory());
 					assertFalse("CleanUpDerivedResources is set correctly", outputConf.isCleanUpDerivedResources());
 					assertFalse("SetDerivedProperty is set correctly", outputConf.isSetDerivedProperty());
 					break;
 				default:
-					assertTrue("All names are correct", false);
+					fail("Found incorrectly named output configuration");
 					break;
 			}
 		}
