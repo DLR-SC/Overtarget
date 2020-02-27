b47b78a779e30d135aec031513bc4c908295d16c
Anna-Lena-DLR <anna-lena.gefke@dlr.de>
2020-02-27 14:27:58 +0100

    Added helper class for handler- (Task #25)
    ---
    Task #25: Resolve target platform from tmodel editor

--- de.dlr.sc.overtarget.language.ui/src/de/dlr/sc/overtarget/language/ui/handler/ResolveTargetHandler.java
+++ de.dlr.sc.overtarget.language.ui/src/de/dlr/sc/overtarget/language/ui/handler/ResolveTargetHandler.java
@@ -10,37 +10,26 @@
 
 package de.dlr.sc.overtarget.language.ui.handler;
 
-
-import java.io.File;
-import java.net.URI;
-import java.nio.file.Path;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.pde.core.target.ITargetDefinition;
-import org.eclipse.pde.core.target.ITargetPlatformService;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.texteditor.ITextEditor;
-import org.eclipse.pde.core.target.LoadTargetDefinitionJob;
-import org.eclipse.pde.internal.core.PDECore;
 
-
+import de.dlr.sc.overtarget.language.util.TargetPlatformHelper;
 
 public class ResolveTargetHandler extends AbstractHandler implements IHandler {
 
-	private ITargetDefinition fTarget;
-
+	static String targetFileExtension = "target";
+	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IEditorPart editorPart = HandlerUtil.getActiveEditor(event);
@@ -51,36 +40,19 @@ public class ResolveTargetHandler extends AbstractHandler implements IHandler {
 			}
 			
 			IFile file = ((FileEditorInput) input).getFile();
-			IPath path = file.getFullPath().removeFileExtension().addFileExtension("target");
-
+			IPath path = file.getFullPath().removeFileExtension().addFileExtension(targetFileExtension);
 
 			IWorkspace workspace = ResourcesPlugin.getWorkspace();	
 			IFile targetFile = workspace.getRoot().getFile(path);
 			
 			if (targetFile.exists()) {
-				ITargetPlatformService service = getTargetPlatformService();
-				org.eclipse.pde.core.target.ITargetHandle fileHandle = service.getTarget(targetFile);
-				ITargetDefinition target = null;
-				try {
-					target = fileHandle.getTargetDefinition();
-				} catch (CoreException e1) {
-					e1.printStackTrace();
-				}
-				LoadTargetDefinitionJob.load(target);
+				TargetPlatformHelper.setAsTargetPlatform(targetFile);
 			}
-		
 		}
 		
 		return null;
 	}
 
-	private ITargetPlatformService getTargetPlatformService() {
-		@SuppressWarnings("restriction")
-		ITargetPlatformService service = PDECore.getDefault().acquireService(ITargetPlatformService.class);
-		
-		return service;
-	}
-	
 	@Override
 	public boolean isEnabled() {
 		return true;
---
+++ de.dlr.sc.overtarget.language/src/de/dlr/sc/overtarget/language/util/TargetPlatformHelper.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2020 German Aerospace Center (DLR), Simulation and Software Technology, Germany.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package de.dlr.sc.overtarget.language.util;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.pde.core.target.ITargetDefinition;
+import org.eclipse.pde.core.target.ITargetPlatformService;
+import org.eclipse.pde.core.target.LoadTargetDefinitionJob;
+import org.eclipse.pde.internal.core.PDECore;
+
+/**
+ * Class to set target platforms from tmodel editor
+ * 
+ */
+
+@SuppressWarnings("restriction")
+public class TargetPlatformHelper {
+
+	
+	private TargetPlatformHelper() {
+	}
+	
+	/**
+	 * @param targetFile
+	 *            the target file to be set
+	*/
+	
+	public static void setAsTargetPlatform(IFile targetFile) {
+		ITargetPlatformService service = getTargetPlatformService();
+		org.eclipse.pde.core.target.ITargetHandle fileHandle = service.getTarget((IFile) targetFile);
+		ITargetDefinition target = null;
+		try {
+			target = fileHandle.getTargetDefinition();
+		} catch (CoreException e1) {
+			e1.printStackTrace();
+		}
+		LoadTargetDefinitionJob.load(target);
+	}
+	
+	private static ITargetPlatformService getTargetPlatformService() {
+		@SuppressWarnings("restriction")
+		ITargetPlatformService service = PDECore.getDefault().acquireService(ITargetPlatformService.class);
+		
+		return service;
+	}
+}
