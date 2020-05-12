b6ccc2871fcb5a482a25dd61d3e520f817ee341e
Anna-Lena-DLR <anna-lena.gefke@dlr.de>
2020-05-12 13:42:13 +0200

    Added error message - (Task #115)
    When the keyword targetLibrary is used, a customized error message
    appears
    
    ---
    Task #115: Bug: Error message when setting a targetLibrary as target has
    to be adjusted

--- de.dlr.sc.overtarget.language.ui/src/de/dlr/sc/overtarget/language/ui/handler/TargetActivationHandler.java
+++ de.dlr.sc.overtarget.language.ui/src/de/dlr/sc/overtarget/language/ui/handler/TargetActivationHandler.java
@@ -10,6 +10,8 @@
 
 package de.dlr.sc.overtarget.language.ui.handler;
 
+import java.lang.annotation.Target;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -17,6 +19,11 @@ import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.MessageBox;
@@ -25,12 +32,24 @@ import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.texteditor.ITextEditor;
+import org.eclipse.xtext.Keyword;
+import org.eclipse.xtext.ParserRule;
+import org.eclipse.xtext.TypeRef;
 import org.eclipse.xtext.builder.EclipseOutputConfigurationProvider;
+import org.eclipse.xtext.nodemodel.ICompositeNode;
+import org.eclipse.xtext.nodemodel.INode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
+import org.eclipse.xtext.ui.resource.IResourceSetProvider;
+import org.w3c.dom.Node;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 
 import de.dlr.sc.overtarget.language.generator.OvertargetGenerator;
+import de.dlr.sc.overtarget.language.services.OvertargetGrammarAccess;
+import de.dlr.sc.overtarget.language.targetmodel.TargetFile;
+import de.dlr.sc.overtarget.language.targetmodel.TargetLibrary;
+import de.dlr.sc.overtarget.language.targetmodel.impl.TargetLibraryImpl;
 import de.dlr.sc.overtarget.language.ui.internal.LanguageActivator;
 import de.dlr.sc.overtarget.language.util.TargetPlatformHelper;
 
@@ -47,7 +66,13 @@ public class TargetActivationHandler extends AbstractHandler implements IHandler
 	}
 
 	@Inject
+	private IResourceSetProvider resourceSetProvider;
+	
+	@Inject
 	private Provider<EclipseOutputConfigurationProvider> eclipseOutputConfigProvider;
+	
+	@Inject 
+	OvertargetGrammarAccess grammarAccess;
 	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
@@ -64,13 +89,32 @@ public class TargetActivationHandler extends AbstractHandler implements IHandler
 			String outputConfig = getOutputConfigurations(project);
 			IFile targetFile;
 			String outputPath = outputConfig.replace(".", "");
+			
+			URI uri = URI.createPlatformResourceURI(file.getFullPath().toString(), true);
+			ResourceSet rs = resourceSetProvider.get(project);
+			Resource r = rs.getResource(uri, true);
+			
+			//to check if the tmodel is a target library
+			TargetFile target = (TargetFile) r.getContents().get(0);
+			ICompositeNode node = NodeModelUtils.getNode(target);
+			String targetLibraryKeyword = grammarAccess.getTargetLibraryRule().getName();
+			String nodeKeyword = node.getFirstChild().getText();
+			boolean isTargetLibrary = nodeKeyword.equals(targetLibraryKeyword);
+			
 			if (outputPath.equals("/")) {
 				targetFile = project.getFile("/" + targetName);
 			} else {
 				String targetPath = outputPath + "/" + targetName;
 				targetFile = project.getFile(targetPath);
 			}
-			if (targetFile.exists()) {
+			if (isTargetLibrary) {
+				MessageBox errorMessage = new MessageBox(
+						Display.getCurrent().getActiveShell(), 
+						SWT.OK | SWT.ICON_ERROR);
+				errorMessage.setText("Error");
+				errorMessage.setMessage("A " + targetLibraryKeyword + " cannot be set as active target.");
+				errorMessage.open();
+			} else if (targetFile.exists()) {
 				try {
 					TargetPlatformHelper targetPlatHelper = new TargetPlatformHelper();
 					targetPlatHelper.setAsActiveTarget(targetFile);
