0a96a196c4bd08ad5fec75d77c327634ac932468
Anna-Lena-DLR <anna-lena.gefke@dlr.de>
2020-05-20 12:01:05 +0200

    tmodel name is corrected - (Task #90)
    Quick fix uses the file name and replaces the wrong written value after
    the keyword Target with the right one
    
    ---
    Task #90: QuickFix: change tmodel name to correct name

--- de.dlr.sc.overtarget.language.ui/xtend-gen/de/dlr/sc/overtarget/language/ui/quickfix/OvertargetQuickfixProvider.java
+++ de.dlr.sc.overtarget.language.ui/xtend-gen/de/dlr/sc/overtarget/language/ui/quickfix/OvertargetQuickfixProvider.java
@@ -9,14 +9,20 @@
  */
 package de.dlr.sc.overtarget.language.ui.quickfix;
 
+import com.google.inject.Inject;
 import de.dlr.sc.overtarget.language.generator.util.ReferencedTargetHelper;
 import de.dlr.sc.overtarget.language.services.OvertargetGrammarAccess;
+import de.dlr.sc.overtarget.language.targetmodel.TargetModel;
 import de.dlr.sc.overtarget.language.ui.handler.GenerationHandler;
 import de.dlr.sc.overtarget.language.util.TargetPlatformHelper;
 import de.dlr.sc.overtarget.language.validation.OvertargetValidator;
-import javax.inject.Inject;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IFileEditorInput;
@@ -29,6 +35,7 @@ import org.eclipse.xtext.ui.editor.model.edit.IModificationContext;
 import org.eclipse.xtext.ui.editor.quickfix.DefaultQuickfixProvider;
 import org.eclipse.xtext.ui.editor.quickfix.Fix;
 import org.eclipse.xtext.ui.editor.quickfix.IssueResolutionAcceptor;
+import org.eclipse.xtext.ui.resource.IResourceSetProvider;
 import org.eclipse.xtext.validation.Issue;
 
 /**
@@ -85,5 +92,41 @@ public class OvertargetQuickfixProvider extends DefaultQuickfixProvider {
           }
         }
       }, 1);
+  }
+  
+  @Inject
+  private IResourceSetProvider resourceSetProvider;
+  
+  @Fix(OvertargetValidator.FILE_NAME_LIKE_TARGET_NAME)
+  public void fixFileNameLikeTargetName(final Issue issue, final IssueResolutionAcceptor acceptor) {
+    final IModification _function = (IModificationContext context) -> {
+      final IEditorPart editor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+      if ((editor instanceof ITextEditor)) {
+        final NullProgressMonitor progressMonitor = new NullProgressMonitor();
+        ((ITextEditor)editor).doSave(progressMonitor);
+        final ITextEditor ite = ((ITextEditor) editor);
+        final IEditorInput input = ite.getEditorInput();
+        final IFileEditorInput fileEditorInput = ((IFileEditorInput) input);
+        final IFile file = fileEditorInput.getFile();
+        final URI uri = URI.createPlatformResourceURI(file.getFullPath().toString(), true);
+        final IProject project = file.getProject();
+        final ResourceSet rs = this.resourceSetProvider.get(project);
+        final Resource r = rs.getResource(uri, true);
+        EObject _get = r.getContents().get(0);
+        final TargetModel model = ((TargetModel) _get);
+        final String modelName = model.getName();
+        final String targetKeyword = this.grammarAccess.getKEYWORDAccess().getTargetKeyword_1().getValue();
+        final String targetLibraryKeyword = this.grammarAccess.getTargetLibraryRule().getName();
+        final int WHITESPACE_SEPARATOR = 1;
+        final IXtextDocument xtextDocument = context.getXtextDocument();
+        final String fileName = ((ITextEditor)editor).getTitle().replace(".tmodel", "");
+        Integer _offset = issue.getOffset();
+        int _plus = ((_offset).intValue() + WHITESPACE_SEPARATOR);
+        int _length = targetKeyword.length();
+        int _plus_1 = (_plus + _length);
+        xtextDocument.replace(_plus_1, modelName.length(), fileName);
+      }
+    };
+    acceptor.accept(issue, "Replace with correct tmodel name", "", "upcase.png", _function);
   }
 }
